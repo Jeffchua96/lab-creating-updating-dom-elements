9e652417184beb29ea1682f7af662b9ea6ae0eb2
Jeffchua96 <jeffersonchua@pursuit.org>
2023-03-19 08:59:24 -0400

    dom elements

---
+++ main.js
@@ -0,0 +1,18 @@
+const body = document.body
+const h1= document.createElement("featured")
+
+
+
+
+const featured = document.querySelector("main-header")
+
+console.log("featured")
+
+
+
+const h4 = document.createElement("img")
+
+
+
+const title=document.getElementById("featured")
+
